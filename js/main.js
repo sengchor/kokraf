f8857f947b1041d0140ac3ff463cfb6383b44b0d
Sengchor <chortaingseng@gmail.com>
2025-05-13 11:02:51 +0700

    Add menu bar

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import { OrbitControls} from "jsm/controls/OrbitControls.js";
 const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(3, 3, 3);
 
 // Renderer
