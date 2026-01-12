4d0f230dccbc8aca6a16b87192cec9b8ab6bb68a
Sengchor <chortaingseng@gmail.com>
2026-01-12 15:52:12 +0700

    Reset credits every month using pg_cron

---
+++ supabase/supabase/functions/reset-credits/index.ts
@@ -0,0 +1,35 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
+const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+
+const PG_CRON_SECRET = Deno.env.get("PG_CRON_SECRET")!;
+
+console.log("Reset Credits Function Loaded!");
+
+Deno.serve(async (req) => {
+  try {
+    const secret = req.headers.get("x-cron-secret");
+    if (secret !== PG_CRON_SECRET) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const { error } = await supabase
+      .from("profiles")
+      .update({ credits: 20 })
+      .not("id", "is", null);;
+
+    if (error) {
+      console.error("Reset failed:", error);
+      return new Response(JSON.stringify({ success: false }), { status: 500 });
+    }
+
+    console.log("Monthly credits reset to 20 for all users");
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ success: false }), { status: 500 });
+  }
+});
