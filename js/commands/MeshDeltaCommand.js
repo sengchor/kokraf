b620841c9e88907198d6874d9d3cbf702d651055
Sengchor <chortaingseng@gmail.com>
2026-06-16 10:18:04 +0700

    Apply meshData delta updates only to modified render buffers

--- js/commands/MeshDeltaCommand.js
+++ js/commands/MeshDeltaCommand.js
@@ -1,4 +1,5 @@
 import { MeshDataRegion } from '../core/MeshDataRegion.js';
+import { MeshRendererAdapter } from '../geometry/MeshRendererAdapter.js';
 
 export class MeshDeltaCommand {
   /**
@@ -34,8 +35,60 @@ export class MeshDeltaCommand {
     if (!object || !delta) return;
 
     this.vertexEditor.setObject(object);
-    MeshDataRegion.apply(this.vertexEditor.meshData, delta);
-    this.vertexEditor.updateGeometryAndHelpers();
+    const meshData = this.vertexEditor.meshData;
+    const renderBuffer = this.vertexEditor.renderBuffer;
+    const geometry = this.vertexEditor.geometry;
+
+    const affectedFaces = new Set();
+    const affectedVertices = new Set();
+
+    for (const [key, data] of Object.entries(delta.faces)) {
+      const id = Number(key);
+
+      if (meshData.faces.has(id)) {
+        MeshRendererAdapter.deleteFace(meshData, renderBuffer, geometry, id);
+      }
+    }
+
+    for (const [key, data] of Object.entries(delta.vertices)) {
+      const id = Number(key);
+      if (data === null && meshData.vertices.has(id)) {
+        MeshRendererAdapter.deleteVertex(meshData, renderBuffer, geometry, id);
+      }
+    }
+
+    MeshDataRegion.apply(meshData, delta);
+
+    for (const [key, data] of Object.entries(delta.vertices)) {
+      const id = Number(key);
+      if (data !== null) {
+        if (!renderBuffer.vertexIdToBufferIndex.has(id)) {
+          MeshRendererAdapter.addVertex(meshData, renderBuffer, geometry, id);
+        }
+
+        const vertex = meshData.vertices.get(id);
+        const slots = renderBuffer.vertexIdToBufferIndex.get(id) || [];
+
+        for (const slot of slots) {
+          geometry.attributes.position.setXYZ(slot, vertex.position.x, vertex.position.y, vertex.position.z);
+        }
+        affectedVertices.add(id);
+      }
+    }
+
+    for (const [key, data] of Object.entries(delta.faces)) {
+      const id = Number(key);
+      if (data !== null) {
+        MeshRendererAdapter.addFace(meshData, renderBuffer, geometry, id);
+        affectedFaces.add(id);
+      }
+    }
+
+    geometry.attributes.position.needsUpdate = true;
+
+    MeshRendererAdapter.updateNormalsForAffectedFaces(meshData, renderBuffer, geometry, affectedFaces, affectedVertices);
+
+    this.signals.editSelectionRefresh.dispatch();
   }
 
   toJSON() {
